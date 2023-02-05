7b17a003d7be3c5ea6f368d821421275a380cf50
rodrigofran <rodrigofrancms@gmail.com>
2023-02-05 13:12:58 -0300

    creating a method to formatter date

--- GitHubApp/Extensions/Date+Extensions.swift
+++ GitHubApp/Extensions/Date+Extensions.swift
@@ -4,8 +4,13 @@ extension Date {
     
     static func format(dateString: String) -> String? {
         
-        // TODO
-        return nil
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+        let date = dateFormatter.date(from: dateString)
+        
+        dateFormatter.dateFormat = "dd/MM/yyyy"
+        
+        return dateFormatter.string(from: date!)
     }
     
     func travelToFuture(years: Int) -> String? {
